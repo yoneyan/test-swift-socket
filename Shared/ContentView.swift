bce6b16cd8c51d6807e3d6e66520a6e1feaf84a5
yoneyan <contact@yoneyan.net>
2022-04-24 17:46:32 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 米田 悠人  on 2022/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestRtkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRtkApp.swift
+//  Shared
+//
+//  Created by 米田 悠人  on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestRtkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
